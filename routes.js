a9da6b11f21ab28eed2c56de0ef61ca3d0641706
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-03-11 08:36:27 -0300

    create route post registerTeacher

--- routes.js
+++ routes.js
@@ -2,15 +2,23 @@ const express = require('express')
 const routes = express.Router()
 
 routes.get('/', function(req, res) {
-    res.redirect('teachers')
+    res.redirect('teachers/index')
 })
 
-routes.get('/teachers', function(req, res) {
-    res.render('teachers')
+routes.get('/teachers/index', function(req, res) {
+    res.render('teachers/index')
+})
+
+routes.post('/teachers', function(req, res) {
+    return res.send('recebido')
+})
+
+routes.get('/teachers/registerTeacher', function(req, res) {
+    return res.render('teachers/registerTeacher')
 })
 
 routes.get('/students', function(req, res) {
-    res.render('students')
+    res.render('students/students')
 })
 
 module.exports = routes
