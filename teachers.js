3d7ba9c1dd7657d15353aafc0bd23ba2a839c44a
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-03-12 23:57:37 -0300

    estruturando dados dos professores

--- teachers.js
+++ teachers.js
@@ -1,11 +1,42 @@
+// importando filesystem do node
+const fs = require("fs")
+const data = require("./data.json")
+
 //exportando arquivo
-exports.post =  function(req, res) {
+exports.post = function (req, res) {
+    //validação
     const keys = Object.keys(req.body)
-    
-    for(key of keys) {
-        if(req.body[key] == '') {
+    for (key of keys) {
+        if (req.body[key] == '') {
             return res.send('Please, fill all fields!')
         }
     }
-    return res.send(req.body)
+
+    // organizacao dos dados a serem enviados
+    let {avatar_url, name, selectedEducation, typeAula, atuacao } = req.body
+
+    // tratamento dos dados
+    let birth = Date.parse(req.body.birth)
+    let created_at = Date.now()
+    let id = Number(data.teachers.length + 1)
+
+
+    data.teachers.push({
+        id,
+        avatar_url,
+        name,
+        birth,
+        selectedEducation,
+        typeAula,
+        atuacao,
+        created_at
+})
+
+fs.writeFile("data.json", JSON.stringify(data, null, 2), function (err) {
+    if (err) {
+        return res.send('Write file error')
+    }
+
+    return res.redirect("/")
+})
 }
