52f1b99b479da41f3d579262bc1b024010d4823c
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-03-17 21:02:23 -0300

    corrigindo nome das rotas

--- routes.js
+++ routes.js
@@ -3,18 +3,18 @@ const routes = express.Router()
 const teachers = require('./teachers')
 
 routes.get('/', function(req, res) {
-    res.redirect('teachers/index')
+    res.redirect('teachers')
 })
 
-routes.get('/teachers/index', function(req, res) {
+routes.get('/teachers', function(req, res) {
     res.render('teachers/index')
 })
 
 //Estrutura de validação do cadastro de professores
 routes.post('/teachers', teachers.post)
 
-routes.get('/teachers/registerTeacher', function(req, res) {
-    return res.render('teachers/registerTeacher')
+routes.get('/teachers/register', function(req, res) {
+    return res.render('teachers/register')
 })
 
 routes.get('/students', function(req, res) {
