e4fd25d3d1f705a6aed16838281efb6166055399
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-04-05 03:34:29 -0300

    criando funcionalidade de editar

--- teachers.js
+++ teachers.js
@@ -102,3 +102,17 @@ exports.put = function(req, res) {
     })
 
 }
+
+exports.delete = function(req, res) {
+    const { id } = req.body
+    const filteredTeachers = data.teachers.filter(function(teacher) {
+        return teacher.id != id
+    })
+
+    data.teachers = filteredTeachers
+
+    fs.writeFile('data.json', JSON.stringify(data, null, 2), function(error) {
+        if (error) return res.send('Write file error')
+        return res.redirect(`/teachers`)
+    })
+}
