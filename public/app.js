318e81cce81fcff90d7b6f30168a3a83b9877ba7
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-06-24 22:29:33 -0300

    feat: modal para confirmar exclusão do professor e aluno

--- public/app.js
+++ public/app.js
@@ -6,3 +6,12 @@ for(item of menuItems) {
         item.classList.add("active")
     } 
 }
+
+// delete confirmation
+const formDelete = document.querySelector("#form-delete")
+formDelete.addEventListener("submit", function(event){
+    const confirmation = confirm("Deseja Deletar?")
+    if(!confirmation) {
+        event.preventDefault()
+    }
+})
