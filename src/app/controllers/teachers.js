ebf9c047d7ea9ce197f84f74c199e99b73fbcec8
gabrielcaiana <gabrielcaiana@hotmail.com>
2020-08-27 21:33:58 -0300

    recuperando dados do banco de dados para exibição por usuário

--- src/app/controllers/teachers.js
+++ src/app/controllers/teachers.js
@@ -23,7 +23,15 @@ module.exports = {
     })
   },
   show(req, res) {
-    return;
+    teachers.find(req.params.id, function(teacher){
+      if(!teacher) return res.send(" Teacher not found!")
+
+      teacher.birth_date = calculatorAge(teacher.birth_date)
+      teacher.subjects_taught = teacher.subjects_taught.split(",")
+      teacher.created_at = date(teacher.created_at).format
+
+      return res.render("teachers/show", { teacher });
+    })
   },
   edit(req, res) {
     return;
--- src/app/models/teacher.js
+++ src/app/models/teacher.js
@@ -48,7 +48,12 @@ module.exports = {
         callback(results.rows[0])
     })
   },
-  find(id, callback) {},
+  find(id, callback) {
+      db.query(`SELECT * FROM teachers WHERE id = $1`, [id], function(err, results){
+      if (err) throw `Database error! ${err}`;
+      callback(results.rows[0])
+      })
+  },
   update(data, callback) {},
   delete(id, callback) {},
 };
